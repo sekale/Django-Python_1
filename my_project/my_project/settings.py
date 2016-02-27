a31f9345aeb80cf949323fedd92f06f6175ecd1c
Siddhant <sekale@purdue.edu>
2016-02-26 20:23:02 -0500

    Javascript and CSS Styles loaded up, STATICFILES_DIR in settings.py fixed

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -91,9 +91,11 @@ os.path.join(os.path.dirname(BASE_DIR), "static","templates"),
 
 
 if DEBUG:
-	MEDIA_URL = '/media/'	
-	STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static","static-only") 
-	MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static","media")
+    MEDIA_URL = '/media/'	
+    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static","static-only") 
+    MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static","media")
+    STATICFILES_DIRS =  (os.path.join(os.path.dirname(BASE_DIR), "static", "static"),)
+    
 	
 
 
