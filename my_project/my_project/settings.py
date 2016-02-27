e81a2da9ecde5a229be8016ed7a8bb6764214a02
Siddhant <sekale@purdue.edu>
2016-02-26 19:35:09 -0500

    Settings Urls modified to store static data i.e.static files and media which user will upload

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -85,5 +85,15 @@ STATIC_URL = '/static/'
 #Template Location
 
 TEMPLATE_DIRS = ( 
-os.path.join(os.path.dirname(BASE_DIR), "static","templates")
+os.path.join(os.path.dirname(BASE_DIR), "static","templates"),
     )
+
+
+
+if DEBUG:
+	MEDIA_URL = '/media/'	
+	STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static","static-only") 
+	MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static","media")
+	
+
+
